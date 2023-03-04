e1b1fc4353d4b8759cc01359f2e326d1045bafc9
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-03-04 22:44:13 +0600

    modal card 2 picture and accuracy button add

--- JSS/app.js
+++ JSS/app.js
@@ -1,17 +1,17 @@
-const dataLoad= ()=> {
-    fetch('https://openapi.programming-hero.com/api/ai/tools')
-    .then(response=> response.json())
-    .then(data=>  showData(data.data.tools.slice(0, 6)))
-}
+const dataLoad = () => {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((response) => response.json())
+    .then((data) => showData(data.data.tools.slice(0, 6)));
+};
 
-const showData=  (aiDatas) =>{
-   const aiDataContainer = document.getElementById('ai-data-container');
-   aiDataContainer.innerText= '';
-   aiDatas.forEach(aiData => {
-    console.log(aiData.id)
-      const dataContainerDiv= document.createElement('div');
-      dataContainerDiv.classList.add('col');
-      dataContainerDiv.innerHTML=`
+const showData = (aiDatas) => {
+  const aiDataContainer = document.getElementById("ai-data-container");
+  aiDataContainer.innerText = "";
+  aiDatas.forEach((aiData) => {
+    console.log(aiData.id);
+    const dataContainerDiv = document.createElement("div");
+    dataContainerDiv.classList.add("col");
+    dataContainerDiv.innerHTML = `
                 <div class="card h-100 p-3">
                       <img src="${aiData.image}" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -29,111 +29,93 @@ const showData=  (aiDatas) =>{
                       </div> 
                         <button  onclick="showAiDetailModal('${aiData.id}')" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#seemoreDetailModal">See More</button>
                     </div>
-                </div>`
-      aiDataContainer.appendChild(dataContainerDiv)
-   });
-   showSpin(false)
-}
+                </div>`;
+    aiDataContainer.appendChild(dataContainerDiv);
+  });
+  showSpin(false);
+};
 
-dataLoad()
+dataLoad();
 //load spinner
-const showSpin= loadSpin => {
-  const spinField= document.getElementById('spinner')
-  if(loadSpin){
-     spinField.classList.remove('d-none')
+const showSpin = (loadSpin) => {
+  const spinField = document.getElementById("spinner");
+  if (loadSpin) {
+    spinField.classList.remove("d-none");
+  } else {
+    spinField.classList.add("d-none");
   }
-  else{
-      spinField.classList.add('d-none')
-  }
-}
-
+};
 
 //show all button
 
-document.getElementById('show-all-btn').addEventListener('click', function(){
-  fetch('https://openapi.programming-hero.com/api/ai/tools')
-    .then(response=> response.json())
-    .then(data=> showData(data.data.tools))
-    const hideButton= document.getElementById("btn-show-all");
-    hideButton.classList.add('d-none')
-    showSpin(true)
-})
+document.getElementById("show-all-btn").addEventListener("click", function () {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((response) => response.json())
+    .then((data) => showData(data.data.tools));
+  const hideButton = document.getElementById("btn-show-all");
+  hideButton.classList.add("d-none");
+  showSpin(true);
+});
 
-
-//show detail in modal 
-const showAiDetailModal= id =>{
-  const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
+//show detail in modal
+const showAiDetailModal = (id) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
   fetch(url)
-  .then(response=> response.json())
-  .then(data=> modalDetail(data.data))
-}
+    .then((response) => response.json())
+    .then((data) => modalDetail(data.data));
+};
 
 //show data on modal
 
-const modalDetail= modalDetail =>{
-  const aiModalDescription= document.getElementById('modal-description')
-  aiModalDescription.innerText= modalDetail.description
+const modalDetail = (modalDetail) => {
+  const aiModalDescription = document.getElementById("modal-description");
+  aiModalDescription.innerText = modalDetail.description;
 
-  const planDetailField= document.getElementById('plan-detail')
-  planDetailField.innerText= modalDetail.pricing[0].plan
+  const planDetailField = document.getElementById("plan-detail");
+  planDetailField.innerText = modalDetail.pricing[0].plan;
 
-  const priceDetailField= document.getElementById('price-detail')
-  priceDetailField.innerText= modalDetail.pricing[0].price
+  const priceDetailField = document.getElementById("price-detail");
+  priceDetailField.innerText = modalDetail.pricing[0].price;
 
-  const planDetailField1= document.getElementById('plan1-detail')
-  planDetailField1.innerText= modalDetail.pricing[1].plan
+  const planDetailField1 = document.getElementById("plan1-detail");
+  planDetailField1.innerText = modalDetail.pricing[1].plan;
 
+  const priceDetailField1 = document.getElementById("price1-detail");
+  priceDetailField1.innerText = modalDetail.pricing[1].price;
 
-  const priceDetailField1= document.getElementById('price1-detail')
-  priceDetailField1.innerText= modalDetail.pricing[1].price
-  
+  const planDetailField2 = document.getElementById("plan2-detail");
+  planDetailField2.innerText = modalDetail.pricing[2].plan;
 
-  const planDetailField2= document.getElementById('plan2-detail')
-  planDetailField2.innerText= modalDetail.pricing[2].plan
-
-
-  const priceDetailField2= document.getElementById('price2-detail')
-  priceDetailField2.innerText= modalDetail.pricing[2].price
-
+  const priceDetailField2 = document.getElementById("price2-detail");
+  priceDetailField2.innerText = modalDetail.pricing[2].price;
 
   // for feature
-  const aiHubFeatureTitle= document.getElementById('ai-hub-feature1')
-  aiHubFeatureTitle.innerText= modalDetail.features['1'].feature_name
+  const aiHubFeatureTitle = document.getElementById("ai-hub-feature1");
+  aiHubFeatureTitle.innerText = modalDetail.features["1"].feature_name;
 
-  const aiHubFeatureTitle2= document.getElementById('ai-hub-feature2')
-  aiHubFeatureTitle2.innerText= modalDetail.features['2'].feature_name
+  const aiHubFeatureTitle2 = document.getElementById("ai-hub-feature2");
+  aiHubFeatureTitle2.innerText = modalDetail.features["2"].feature_name;
 
-  const aiHubFeatureTitle3= document.getElementById('ai-hub-feature3')
-  aiHubFeatureTitle3.innerText= modalDetail.features['3'].feature_name
-
+  const aiHubFeatureTitle3 = document.getElementById("ai-hub-feature3");
+  aiHubFeatureTitle3.innerText = modalDetail.features["3"].feature_name;
 
   // for intregation
-  const aiHubIntegrationsTitle1= document.getElementById('ai-hub-Integrations1')
-  aiHubIntegrationsTitle1.innerText= modalDetail.integrations[0]
+  const aiHubIntegrationsTitle1 = document.getElementById("ai-hub-Integrations1");
+  aiHubIntegrationsTitle1.innerText = modalDetail.integrations[0];
 
-  const aiHubIntegrationsTitle2= document.getElementById('ai-hub-Integrations2')
-  aiHubIntegrationsTitle2.innerText= modalDetail.integrations[1]
-  const aiHubIntegrationsTitle3= document.getElementById('ai-hub-Integrations3')
-  aiHubIntegrationsTitle3.innerText= modalDetail.integrations[2]
-
+  const aiHubIntegrationsTitle2 = document.getElementById("ai-hub-Integrations2");
+  aiHubIntegrationsTitle2.innerText = modalDetail.integrations[1];
+  const aiHubIntegrationsTitle3 = document.getElementById("ai-hub-Integrations3");
+  aiHubIntegrationsTitle3.innerText = modalDetail.integrations[2];
 
   // card-2 image and feature
-  const imageCardbody= document.getElementById('image-card-body')
-  const imagecardDiv= document.createElement('div');
-  imagecardDiv.classList.add('card')
-  imagecardDiv.innerHTML=`
-  <div>
-  <div class="card text-bg-dark">
-      <img src="${modalDetail.image_link[0]}" class="card-img" alt="...">
-      <div class="card-img-overlay">
-          <button class="btn btn-danger float-end">${modalDetail.accuracy.score}</button>
-      </div>
-  </div>
-</div>
-<div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">This is a longer card with supporting text below as
-      a natural lead-in to additional content.</p>
-</div>`
-  imageCardbody.appendChild(imagecardDiv)
-}
+  const imageBody= document.getElementById('image-body')
+  imageBody.innerHTML=`
+  <img src="${modalDetail.image_link[0]}" class="card-img" alt="...">`
+
+  const btnAccurecy= document.getElementById('btn-accuracy')
+  btnAccurecy.innerText= modalDetail.accuracy.score ? modalDetail.accuracy.score * 100 : 'no'
+
+  const imageText= document.getElementById('image-text')
+  imageText.innerText= modalDetail.input_output_examples[0].input 
+};
