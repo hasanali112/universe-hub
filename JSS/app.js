6bef6a724ce732dddcd5a32dbb4f3a6b68bc315c
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-03-04 21:06:32 +0600

    modal body informaton add

--- JSS/app.js
+++ JSS/app.js
@@ -8,6 +8,7 @@ const showData=  (aiDatas) =>{
    const aiDataContainer = document.getElementById('ai-data-container');
    aiDataContainer.innerText= '';
    aiDatas.forEach(aiData => {
+    console.log(aiData.id)
       const dataContainerDiv= document.createElement('div');
       dataContainerDiv.classList.add('col');
       dataContainerDiv.innerHTML=`
@@ -26,7 +27,7 @@ const showData=  (aiDatas) =>{
                         <h4 class="text-bolder">${aiData.name}</h4>
                         <p><span><i class="fa-solid fa-calendar-days"></i></span> ${aiData.published_in}</p>
                       </div> 
-                        <button href="#" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#seemoreDetailModal">See More</button>
+                        <button  onclick="showAiDetailModal('${aiData.id}')" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#seemoreDetailModal">See More</button>
                     </div>
                 </div>`
       aiDataContainer.appendChild(dataContainerDiv)
@@ -57,3 +58,82 @@ document.getElementById('show-all-btn').addEventListener('click', function(){
     hideButton.classList.add('d-none')
     showSpin(true)
 })
+
+
+//show detail in modal 
+const showAiDetailModal= id =>{
+  const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
+  fetch(url)
+  .then(response=> response.json())
+  .then(data=> modalDetail(data.data))
+}
+
+//show data on modal
+
+const modalDetail= modalDetail =>{
+  const aiModalDescription= document.getElementById('modal-description')
+  aiModalDescription.innerText= modalDetail.description
+
+  const planDetailField= document.getElementById('plan-detail')
+  planDetailField.innerText= modalDetail.pricing[0].plan
+
+  const priceDetailField= document.getElementById('price-detail')
+  priceDetailField.innerText= modalDetail.pricing[0].price
+
+  const planDetailField1= document.getElementById('plan1-detail')
+  planDetailField1.innerText= modalDetail.pricing[1].plan
+
+
+  const priceDetailField1= document.getElementById('price1-detail')
+  priceDetailField1.innerText= modalDetail.pricing[1].price
+  
+
+  const planDetailField2= document.getElementById('plan2-detail')
+  planDetailField2.innerText= modalDetail.pricing[2].plan
+
+
+  const priceDetailField2= document.getElementById('price2-detail')
+  priceDetailField2.innerText= modalDetail.pricing[2].price
+
+
+  // for feature
+  const aiHubFeatureTitle= document.getElementById('ai-hub-feature1')
+  aiHubFeatureTitle.innerText= modalDetail.features['1'].feature_name
+
+  const aiHubFeatureTitle2= document.getElementById('ai-hub-feature2')
+  aiHubFeatureTitle2.innerText= modalDetail.features['2'].feature_name
+
+  const aiHubFeatureTitle3= document.getElementById('ai-hub-feature3')
+  aiHubFeatureTitle3.innerText= modalDetail.features['3'].feature_name
+
+
+  // for intregation
+  const aiHubIntegrationsTitle1= document.getElementById('ai-hub-Integrations1')
+  aiHubIntegrationsTitle1.innerText= modalDetail.integrations[0]
+
+  const aiHubIntegrationsTitle2= document.getElementById('ai-hub-Integrations2')
+  aiHubIntegrationsTitle2.innerText= modalDetail.integrations[1]
+  const aiHubIntegrationsTitle3= document.getElementById('ai-hub-Integrations3')
+  aiHubIntegrationsTitle3.innerText= modalDetail.integrations[2]
+
+
+  // card-2 image and feature
+  const imageCardbody= document.getElementById('image-card-body')
+  const imagecardDiv= document.createElement('div');
+  imagecardDiv.classList.add('card')
+  imagecardDiv.innerHTML=`
+  <div>
+  <div class="card text-bg-dark">
+      <img src="${modalDetail.image_link[0]}" class="card-img" alt="...">
+      <div class="card-img-overlay">
+          <button></button>
+      </div>
+  </div>
+</div>
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">This is a longer card with supporting text below as
+      a natural lead-in to additional content.</p>
+</div>`
+  imageCardbody.appendChild(imagecardDiv)
+}
